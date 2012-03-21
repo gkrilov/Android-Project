9509c5f73faa01fc6bfea3a961da20997b13abeb
Greg Krilov <gkrilov@gmail.com>
2012-03-21 16:59:34 -0400

    Initial commit

---
+++ src/finalProject/gregKrilov/MainMenu.java
@@ -0,0 +1,17 @@
+package finalProject.gregKrilov;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_menu);
+	}
+
+	
+	
+}
